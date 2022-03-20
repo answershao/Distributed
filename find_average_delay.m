6cdfba02233100f941388d584909ad94c52c3c5e
shaopf <shaopf@buaa.edu.cn>
2022-03-20 23:56:45 +0800

    fixed version

--- find_average_delay.m
+++ find_average_delay.m
@@ -1,4 +1,4 @@
-function [average_delay_duration, total_delay] = find_average_delay(CPM,finally_tocal_duration,ad,L,delay)
+function [average_delay_duration] = find_average_delay(CPM,finally_tocal_duration,ad,L,delay)
 delay_duration = [];
 delay_duration_s = [];
 for i =1:L
@@ -6,6 +6,6 @@ for i =1:L
     delay_duration_s  = [delay_duration_s ;delay_duration(i)];
 end
 
-total_delay = delay*delay_duration_s;
+% total_delay = delay*delay_duration_s;
 average_delay_duration =ceil(sum(delay_duration_s)/L);
 % 
